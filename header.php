154bfe886e43745d76355e58dd24160e80d7b798
suraj kumar <surajkumar54985@gmail.com>
2024-01-04 16:31:23 +0530

    Added dropdown menu for smaller screens

--- header.php
+++ header.php
@@ -1,39 +1,42 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>  </title>
-	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-	<script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <title>Hospital Management System</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-info bg-dark">
-		<h4 class="text-white"><span style="font-family:'Aclonica' font-size:22px"><img src="img/logo.png" width="30" height="30">
-  Hospital Management System&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</h4>
-		<div class="mr-auto"></div>
-		<ul class="navbar-nav">
-			<?php
-			if (isset($_SESSION['admin'])) {
-				$user=$_SESSION['admin'];
-				echo '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<li class="nav-item"><a href="#" class="nav-link text-white">@User</li></a>&emsp;&emsp;
-			<li class="nav-item"><a href="logout.php" class="nav-link text-white">logout</a></li>';
-			}elseif (isset($_SESSION['doc'])) {
-				$user=$_SESSION['doc'];
-				echo '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<li class="nav-item"><a href="#" class="nav-link text-white">@User</li></a>&emsp;&emsp;
-			<li class="nav-item"><a href="logout.php" class="nav-link text-white">logout</a></li>';
-			}elseif (isset($_SESSION['patient'])) {
-				$user=$_SESSION['patient'];
-				echo '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<li class="nav-item"><a href="#" class="nav-link text-white">@User</li></a>&emsp;&emsp;
-			<li class="nav-item"><a href="logout.php" class="nav-link text-white">logout</a></li>';
-			}
-			else{
-				echo'<li class="nav-item"><a href="index.php" class="nav-link text-white"><img src="img/home.png" width="20" height="20" alt="">HOME</a></li>
-				<li class="nav-item"><a href="adminlogin.php" class="nav-link text-white"><img src="img/admin.png" width="20" height="20" alt="">ADMIN</a></li>
-			<li class="nav-item"><a href="doctorlogin.php" class="nav-link text-white"><img src="img/doctor.png" width="20" height="20" alt="">DOCTOR</a></li>
-			<li class="nav-item"><a href="patientlogin.php" class="nav-link text-white"><img src="img/patient.jpg" width="20" height="20" alt="">PATIENT</a></li>';
-			}?>
-		</ul>
-	</nav>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="Hospital Management System Logo">
+            HMS
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <?php
+                if (isset($_SESSION['admin']) || isset($_SESSION['doc']) || isset($_SESSION['patient'])) {
+                    $user = isset($_SESSION['admin']) ? $_SESSION['admin'] : (isset($_SESSION['doc']) ? $_SESSION['doc'] : $_SESSION['patient']);
+                    echo '<li class="nav-item"><a href="#" class="nav-link text-white">@' . $user . '</a></li>
+                          <li class="nav-item"><a href="logout.php" class="nav-link text-white">Logout</a></li>';
+                } else {
+                    echo '<li class="nav-item"><a href="index.php" class="nav-link text-white"><img src="img/home.png" width="20" height="20" alt=""> HOME</a></li>
+                          <li class="nav-item"><a href="adminlogin.php" class="nav-link text-white"><img src="img/admin.png" width="20" height="20" alt=""> ADMIN</a></li>
+                          <li class="nav-item"><a href="doctorlogin.php" class="nav-link text-white"><img src="img/doctor.png" width="20" height="20" alt=""> DOCTOR</a></li>
+                          <li class="nav-item"><a href="patientlogin.php" class="nav-link text-white"><img src="img/patient.jpg" width="20" height="20" alt=""> PATIENT</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-
+<!-- 
 <!DOCTYPE html>
 <html>
 <head>
@@ -38,6 +38,113 @@
 	
 
 
+
+</body>
+</html> -->
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HMS Home Page</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+    <?php include 'header.php' ?>
+
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <div class="card shadow">
+                    <img src="img/doc.jpg" class="card-img-top" alt="Doctor Image">
+                    <div class="card-body">
+                        <h5 class="card-title text-center text-dark bg-light">Register yourself as a Doctor👨‍⚕️</h5>
+                        <a href="apply.php" class="btn btn-success btn-block">Register!!!</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <div class="card shadow">
+                    <img src="img/pat.jpg" class="card-img-top" alt="Patient Image">
+                    <div class="card-body">
+                        <h5 class="card-title text-center text-dark bg-light">Create a patient account for online appointments🤹‍♀️</h5>
+                        <a href="account.php" class="btn btn-success btn-block">Create Account!!!</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <div class="card shadow">
+                    <img src="img/cont.jpg" class="card-img-top" alt="Contact Image">
+                    <div class="card-body">
+                        <h5 class="card-title text-center text-dark bg-light">Click below to contact us🤝</h5>
+                        <a href="contact.html" class="btn btn-success btn-block">Contact!!!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HMS Home Page</title>
+    <!-- Add Bootstrap CSS link here -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        /* Custom styles for fixing image sizes */
+        .card-img-top {
+            object-fit: cover;
+            height: 350px; /* Set the desired height for the images */
+        }
+    </style>
+</head>
+<body>
+
+    <?php include 'header.php' ?>
+
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <div class="card shadow">
+                    <img src="img/doc.jpg" class="card-img-top" alt="Doctor Image">
+                    <div class="card-body">
+                        <h5 class="card-title text-center text-dark bg-light">Register yourself as a Doctor👨‍⚕️</h5>
+                        <a href="apply.php" class="btn btn-success btn-block">Register!!!</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <div class="card shadow">
+                    <img src="img/pat.jpg" class="card-img-top" alt="Patient Image">
+                    <div class="card-body">
+                        <h5 class="card-title text-center text-dark bg-light">Register yourself for consultation🤹‍♀️</h5>
+                        <a href="account.php" class="btn btn-success btn-block">Create Account!!!</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <div class="card shadow">
+                    <img src="img/cont.jpg" class="card-img-top" alt="Contact Image">
+                    <div class="card-body">
+                        <h5 class="card-title text-center text-dark bg-light">Click below to contact us🤝</h5>
+                        <a href="contact.html" class="btn btn-success btn-block">Contact!!!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
